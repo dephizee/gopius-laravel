03e06a8fbc765232ae5ec1481c7ed9a95a5b9845
dephizee <hafiz227@gmail.com>
2021-03-02 02:32:07 +0100

    init sql batch

--- database/migrations/2021_02_10_191746_create_blocks_table.php
+++ database/migrations/2021_02_10_191746_create_blocks_table.php
@@ -16,7 +16,7 @@ class CreateBlocksTable extends Migration
         Schema::create('blocks', function (Blueprint $table) {
             $table->id('block_id');
             $table->string('block_title');
-            $table->text('block_content')->default("");
+            $table->text('block_content');
             $table->timestamps();
             $table->unsignedBigInteger('chapter_no');
             $table->foreign('chapter_no')->references('chapter_id')->on('chapters');
